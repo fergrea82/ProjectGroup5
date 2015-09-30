317c9313dd397c57857bb6ab4332f8491b49d764
CSung998 <chris_syk@hotmail.co.uk>
2015-09-30 16:25:05 +0100

    AddingNewPOJOS
    Adding new table objects and creating relationships between tables

---
+++ ProjectBeans/ejbModule/objects/dataobjects/CompanyObject.java
@@ -0,0 +1,45 @@
+package objects.dataobjects;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
+@Entity(name="company")
+public class CompanyObject {
+
+	public CompanyObject() {
+		//super();
+	}
+	
+	@Id
+	private int companyID;
+	private String companySymbol;
+	
+	@OneToMany(mappedBy="companyObj", fetch=FetchType.EAGER)
+	private List<StockObject> stocks;
+	
+	public CompanyObject(int companyID, String companySymbol){
+		this.companyID=companyID;
+		this.companySymbol=companySymbol;
+	}
+
+	public int getCompanyID() {
+		return companyID;
+	}
+
+	public void setCompanyID(int companyID) {
+		this.companyID = companyID;
+	}
+
+	public String getCompanySymbol() {
+		return companySymbol;
+	}
+
+	public void setCompanySymbol(String companySymbol) {
+		this.companySymbol = companySymbol;
+	}
+}
